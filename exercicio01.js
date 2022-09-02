2c8d476f91c26149865c117aba257d8b9afe7ed1
AfranioCouto <112716977+AfranioCouto@users.noreply.github.com>
2022-09-02 14:30:20 -0300

    Add files via upload

---
+++ exercicio01.js
@@ -0,0 +1,20 @@
+
+
+function descricao (nome, idade, cidade){
+    
+    console.log(`Olá amigos OiDevs!!
+Me chamo ${nome}. Tenho ${idade} anos. Sou de ${cidade}.
+Sou casado com Sara e pai da Beatriz. Sou muito grato a Deus pela família que tenho.
+Busco essa transição de carreira com o objetivo de crescimento profissional e para
+poder realizar todos os sonhos de minha família.
+Estou bastante decidido e focado nesse novo caminho!!`)
+}
+
+descricao ("Afranio Gonçalves", 39, "Teresina-Piauí")
+
+
+/*    Segunda forma de concatenação:
+    console.log("Olá amigos OiDevs!!")
+    console.log("Me chamo " + nome + ". Tenho " + idade + " anos. Sou de " + cidade + ".")
+    console.log("Sou casado com Sara e pai da Beatriz. Sou muito grato a Deus pela família que tenho. Busco essa transição de carreira com o objetivo de crescimento profissional e para poder realizar todos os sonhos de minha família. Estou bastante decidido e focado nesse novo caminho!!")
+*/
